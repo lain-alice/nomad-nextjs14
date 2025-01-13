995238c9a0dc990f5225e9bf7334f867194bc11a
lain-alice <freya6829@gmail.com>
2025-01-13 22:28:06 +0900

    credits, provider

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,8 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
-import { getMovie } from "../../../../components/movie-info";
+import MovieCredits from "../../../../components/movie-credits";
+import MovieProvider from "../../../../components/movie-provider";
 
 interface IParams {
   params: { id: string };
@@ -9,16 +10,23 @@ interface IParams {
 
 export async function generateMetadata({ params: { id } }: IParams) {
   const movie = await getMovie(id);
+  // getMovie 2번 호출해도 됨, 요청 캐시되니까
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetail({ params: { id } }: IParams) {
+export default async function MovieDetailPage({ params: { id } }: IParams) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie providers</h1>}>
+        <MovieProvider id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie credits</h1>}>
+        <MovieCredits id={id} />
       </Suspense>
       <Suspense fallback={<h1>Loading movie videos</h1>}>
         <MovieVideos id={id} />
