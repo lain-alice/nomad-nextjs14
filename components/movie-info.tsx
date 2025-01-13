1eb8d564bb83b0ccf0c451a58f1df46bb1defafc
lain-alice <freya6829@gmail.com>
2025-01-13 21:56:40 +0900

    movie info, videos

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-info.module.css";
 
 async function getMovie(id: string) {
   console.log(`Fetching Movies : ${Date.now()}`);
@@ -9,5 +10,22 @@ async function getMovie(id: string) {
 
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={styles.container}>
+      <img
+        src={movie.poster_path}
+        className={styles.poster}
+        alt={movie.title}
+      />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>* {movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={"_blank"} rel="noreferrer">
+          Homepage &rarr;
+        </a>
+        {/* target blank: 다른 탭에서 열기 */}
+      </div>
+    </div>
+  );
 }
