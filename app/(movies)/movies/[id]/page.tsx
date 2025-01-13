8d3c010f35d5a0a6cb4cd1c4dccd3f28ab68d539
lain-alice <freya6829@gmail.com>
2025-01-13 22:51:38 +0900

    credit provider delete

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,6 @@
 import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
-import MovieCredits from "../../../../components/movie-credits";
-import MovieProvider from "../../../../components/movie-provider";
 
 interface IParams {
   params: { id: string };
@@ -21,12 +19,6 @@ export default async function MovieDetailPage({ params: { id } }: IParams) {
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
-      </Suspense>
-      <Suspense fallback={<h1>Loading movie providers</h1>}>
-        <MovieProvider id={id} />
-      </Suspense>
-      <Suspense fallback={<h1>Loading movie credits</h1>}>
-        <MovieCredits id={id} />
       </Suspense>
       <Suspense fallback={<h1>Loading movie videos</h1>}>
         <MovieVideos id={id} />
